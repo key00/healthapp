03cf9f8fef7f56033875afba24ce437022fecb78
key00 <kinamugisha@gmail.com>
2023-02-14 15:41:24 +0200

    health app

---
+++ dashboard/doctor/js/index.js
@@ -0,0 +1,8 @@
+(function ($) {
+  "use strict";
+
+  $(".log_out").click(function () {
+    sessionStorage.removeItem("uid");
+    location.href = "../../login/";
+  });
+})(jQuery);
---
+++ dashboard/doctor/js/sidebar.js
@@ -0,0 +1,45 @@
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function () {
+  sidebar.classList.toggle("active");
+  if (sidebar.classList.contains("active")) {
+    sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-left");
+  } else sidebarBtn.classList.replace("bx-menu-alt-left", "bx-menu");
+};
+
+function view() {
+  document.getElementById("a").setAttribute("style", "display:block");
+}
+
+(function ($) {
+  "use strict";
+
+  $(".content").each(function () {
+    $(this).click(function () {
+      if (!$(this).hasClass("active")) {
+        $(".content").each(function () {
+          $(this).removeClass("active");
+        });
+        $(".dashboard").each(function () {
+          $(this).removeClass("active");
+        });
+        $(this).addClass("active");
+
+        if ($(this).hasClass("active")) {
+          if ($(this).hasClass("dash")) {
+            $(".box-content.dashboard").addClass("active");
+          }
+          if ($(this).hasClass("home")) {
+            $(".home.dashboard").addClass("active");
+          }
+          if ($(this).hasClass("patient")) {
+            $(".patient.dashboard").addClass("active");
+          }
+          if ($(this).hasClass("schedule")) {
+            $(".schedule.dashboard").addClass("active");
+          }
+        }
+      }
+    });
+  });
+})(jQuery);
